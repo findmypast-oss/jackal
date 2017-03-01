a159390692c5f5146b8d518d1470860a8937ad91
Donald Robertson <drobertson@findmypast.com>
2017-03-01 13:49:52 +0000

    Moved functionality to lib folder to simplify npm test command, added validator to bring together validation functionality. Added basic server with logging and npm start to run in dev mode

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 'use strict'
 
-const logging = require('./middleware/logging')
 const express = require('express')
+
+const { cache, validate } = require('../contract')
+
+const logging = require('./middleware/logging')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(logging)
+app.use(bodyParser.json())
 
 app.get('/health', function (req, res) {
   res.send('😊')
+})
+
+app.post('/api/contracts', function (req, res) {
+  const { hash, contracts } = req.body
+
+  const contractCache = cache.getCollection('contracts')
+  if (!contractCache.findOne({ 'hash' : { '$eq' : hash } })) {
+    const validations = contracts.map(validate)
+  }
+
 })
 
 app.listen(25863)
