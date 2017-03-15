34490832826cddf8128c8192eb35942733a98aef
Donald Robertson <drobertson@findmypast.com>
2017-03-15 17:04:11 +0000

    Fixing bugs, still some left, they will be squashed, oh yes, they will be squashed

--- server/middleware/claude/index.js
+++ server/middleware/claude/index.js
@@ -6,22 +6,22 @@ const claude = (req, res, next) => {
   const provider = req.params.provider
 
   const contracts = retrieveCollection(provider)
+  console.log(contracts)
+
   if (contracts.length === 0) {
     res.send('No contracts exist for provider')
     next()
+  } else {
+    execute(contracts, (err, results) => {
+      if (results.every(r => r.err === null)) {
+        res.status(200).send('Contracts executed successfully')
+        next()
+      } else {
+        res.status(400).send('Contracts failed to execute')
+        next()
+      }
+    })
   }
-
-  console.log(contracts)
-
-  execute(contracts, (err, results) => {
-    if (results.every(r => r.err === null)) {
-      res.status(200).send('Contracts executed successfully')
-      next()
-    } else {
-      res.status(400).send('Contracts failed to execute')
-      next()
-    }
-  })
 }
 
 module.exports = claude
