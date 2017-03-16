951b50df470e284aa1cb2b2bb620d58292690f61
Donald Robertson <drobertson@findmypast.com>
2017-03-16 09:17:41 +0000

    Squashed bugs. Contracts can now be verified by consumer and provider

--- server/middleware/claude/index.js
+++ server/middleware/claude/index.js
@@ -5,19 +5,18 @@ const { execute, retrieveCollection } = require('../../../lib/contract')
 const claude = (req, res, next) => {
   const provider = req.params.provider
 
-  const contracts = retrieveCollection(provider)
-  console.log(contracts)
+  const contracts = retrieveCollection(provider).map(co => co.contract)
 
   if (contracts.length === 0) {
-    res.send('No contracts exist for provider')
+    res.send(`No contracts exist for provider: ${provider}`)
     next()
   } else {
     execute(contracts, (err, results) => {
       if (results.every(r => r.err === null)) {
-        res.status(200).send('Contracts executed successfully')
+        res.status(200).send(`Contracts executed successfully for provider ${provider}`)
         next()
       } else {
-        res.status(400).send('Contracts failed to execute')
+        res.status(400).send(`Contracts failed to execute for provider ${provider}`)
         next()
       }
     })
