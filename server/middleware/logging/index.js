387996cd3b6c05f5c58ae5d7174c230d9e71ff3a
Donald Robertson <drobertson@findmypast.com>
2017-03-20 14:05:14 +0000

    Extracted logger to separate module to be required in by middleware

--- server/middleware/logging/index.js
+++ server/middleware/logging/index.js
@@ -1,16 +1,7 @@
 'use strict'
 
 const expressPino = require('express-pino-logger')
-
-const pino = require('pino')
-const inst = pino({
-  name: 'jackal'
-})
-
-const logger = inst.child({
-  environment: 'dev',
-  colour: 'black'
-})
+const logger = require('../../../lib/logger')
 
 module.exports = expressPino({
   logger: logger
