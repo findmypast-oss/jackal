36427e170e6463b5f6aa466e874a3c9ab6301f1e
Donald Robertson <drobertson@findmypast.com>
2017-03-20 15:46:23 +0000

    Added return early and headers to middleware

--- server/middleware/claude/index.js
+++ server/middleware/claude/index.js
@@ -4,23 +4,21 @@ const { execute, retrieveCollection } = require('../../../lib/contract')
 
 const claude = (req, res, next) => {
   const provider = req.params.provider
-
   const contracts = retrieveCollection(provider).map(co => co.contract)
 
   if (contracts.length === 0) {
-    res.send(`No contracts exist for provider: ${provider}`)
-    next()
+    res.status(200).send(`No contracts exist for provider: ${provider}`)
   } else {
     execute(contracts, (err, results) => {
       if (results.every(r => r.err === null)) {
         res.status(200).send(`Contracts executed successfully for provider ${provider}`)
-        next()
       } else {
         res.status(400).send(`Contracts failed to execute for provider ${provider}`)
-        next()
       }
     })
   }
+
+  return next()
 }
 
 module.exports = claude
