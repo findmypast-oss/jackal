61469d5f0de7008fce3c692a7ae0ae27a25445e2
Neil Crawford <neil.crawford@gmail.com>
2017-03-30 16:56:21 +0100

    Tidying up integration tests

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -3,94 +3,64 @@ const provider = require('./support/provider')
 const jackal = require('./support/jackal')
 const client = require('./support/client')
 
-describe('Given a jackal and a provider', function() {
-  before(function(done) {
-    jackal.start(done)
-  })
+describe('Testing jackal and a provider', function() {
 
-  after(function(done) {
-    async.parallel([jackal.stop, provider.stop], done)
-  })
+  describe('Happy path tests', function(){
 
-  it('The provider should start successfully', function(done){
-    provider.start({
-      port: 5000,
-      contract: { version: '1' }
-    }, done)
-  })
+    before(jackal.start)
+    after(jackal.stop)
+    after(provider.stop)
 
-  it('Sending contract-v1 the first time should pass', function(done) {
-    client.send({
-      filePath: 'test/integration/contract-v1.json',
-      isPass: true
-    }, done)
-  })
+    it('The provider should start successfully', function(done){
+      provider.start({ port: 5000, contract: { version: '1' } }, done)
+    })
 
-  it('Sending contract-v1 a second time should pass', function(done) {
-    client.send({
-      filePath: 'test/integration/contract-v1.json',
-      isPass: true
-    }, done)
-  })
+    it('Sending contract-v1 the first time should pass', function(done) {
+      client.send({ filePath: 'test/integration/contract-v1.json', isPass: true }, done)
+    })
 
-  it('Sending contract-v1 a third time should pass', function(done) {
-    client.send({
-      filePath: 'test/integration/contract-v1.json',
-      isPass: true
-    }, done)
-  })
+    it('Sending contract-v1 a second time should pass', function(done) {
+      client.send({ filePath: 'test/integration/contract-v1.json', isPass: true }, done)
+    })
 
-  it('Jackal should have hit "/contract" 3 times', function() {
-    expect(provider.contractHitCount()).to.equal(3)
-  })
+    it('Sending contract-v1 a third time should pass', function(done) {
+      client.send({ filePath: 'test/integration/contract-v1.json', isPass: true }, done)
+    })
 
-  it('The provider should stop successfully', function(done) {
-    provider.stop(done)
-  })
+    it('Jackal should have hit "/contract" 3 times', function() {
+      expect(provider.contractHitCount()).to.equal(3)
+    })
 
-  it('The provider should start successfully with contract-v2', function(done){
-    provider.start({
-      port: 5000,
-      contract: { version: 'abc' }
-    }, done)
-  })
+    it('The provider should stop successfully', function(done) {
+      provider.stop(done)
+    })
 
-  it('The existing contract-v1 should fail for the provider with contract-v2', function(done){
-    client.run({
-      provider: 'integration',
-      isPass: false
-    }, done)
-  })
+    it('The provider should start successfully with contract-v2', function(done){
+      provider.start({ port: 5000, contract: { version: 'abc' } }, done)
+    })
 
-  it('Sending contract-v2 should pass', function(done) {
-    client.send({
-      filePath: 'test/integration/contract-v2.json',
-      isPass: true
-    }, done)
-  })
+    it('The existing contract-v1 should fail for the provider with contract-v2', function(done){
+      client.run({ provider: 'integration', isPass: false }, done)
+    })
 
-  it('Running contract-v2 the first time should pass', function(done) {
-    client.run({
-      provider: 'integration',
-      isPass: true
-    }, done)
-  })
+    it('Sending contract-v2 should pass', function(done) {
+      client.send({ filePath: 'test/integration/contract-v2.json', isPass: true }, done)
+    })
 
-  it('Running contract-v2 a second time should pass', function(done) {
-    client.run({
-      provider: 'integration',
-      isPass: true
-    }, done)
-  })
+    it('Running contract-v2 the first time should pass', function(done) {
+      client.run({ provider: 'integration', isPass: true }, done)
+    })
 
-  it('Running contract-v2 a third time should pass', function(done) {
-    client.run({
-      provider: 'integration',
-      isPass: true
-    }, done)
-  })
+    it('Running contract-v2 a second time should pass', function(done) {
+      client.run({ provider: 'integration', isPass: true }, done)
+    })
 
-  it('Jackal should have hit "/contract" 5 times', function() {
-    expect(provider.contractHitCount()).to.equal(5)
+    it('Running contract-v2 a third time should pass', function(done) {
+      client.run({ provider: 'integration', isPass: true }, done)
+    })
+
+    it('Jackal should have hit "/contract" 5 times', function() {
+      expect(provider.contractHitCount()).to.equal(5)
+    })
   })
 })
