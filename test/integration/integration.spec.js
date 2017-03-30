b0fc4691d0b5f93e8809bfccdbe9e761146610fe
Neil Crawford <neil.crawford@gmail.com>
2017-03-30 10:48:18 +0100

    Adding send-x3 and run-x3 end to end tests

--- test/integration/integration.spec.js
+++ test/integration/integration.spec.js
@@ -3,6 +3,37 @@ const provider = require('./test-provider')
 const jackal = require('./test-jackal')
 const consumer = require('./../../client')
 
+function sendContractV1(done) {
+  consumer.send(
+    'test/integration/contract-v1.json',
+    'http://localhost:25863/api/contracts',
+    function (err, response, body) {
+      expect(err).to.not.exist
+      expect(response.statusCode).to.equal(201)
+
+      const parsed = JSON.parse(body)
+      expect(parsed[0].status).to.equal('Pass')
+
+      done()
+    }
+  )
+}
+
+function runContract(done) {
+  consumer.run(
+    'http://localhost:25863/api/contracts/integration',
+    function (err, response, body) {
+      expect(response.statusCode).to.equal(200)
+
+      const parsed = JSON.parse(body)
+      expect(parsed[0].status).to.equal('Pass')
+      expect(parsed[0].error).to.not.exist
+
+      done()
+    }
+  )
+}
+
 describe('Given a jackal and a provider', function() {
   var server = null
 
@@ -21,34 +52,30 @@ describe('Given a jackal and a provider', function() {
     }, done)
   })
 
-  it('The first contract should pass', function(done) {
-    consumer.send(
-      'test/integration/contract-v1.json',
-      'http://localhost:25863/api/contracts',
-      function (err, response, body) {
-        expect(err).to.not.exist
-        expect(response.statusCode).to.equal(201)
+  it('Sending contract-v1 the first time should pass', function(done) {
+    sendContractV1(done)
+  })
 
-        const parsed = JSON.parse(body)
-        expect(parsed[0].status).to.equal('Pass')
+  it('Sending contract-v1 a second time should pass', function(done) {
+    sendContractV1(done)
+  })
 
-        done()
-      }
-    )
+  it('Sending contract-v1 a third time should pass', function(done) {
+    sendContractV1(done)
   })
 
   it('The provider should stop successfully', function(done) {
     provider.stop(done)
   })
 
-  it('The provider should start successfully with contract v2', function(done){
+  it('The provider should start successfully with contract-v2', function(done){
     provider.start({
       port: 5000,
       contract: { version: 'abc' }
     }, done)
   })
 
-  it('The contract should fail for the new provider', function(done){
+  it('The existing contract-v1 should fail for the provider-v2', function(done){
     consumer.run(
       'http://localhost:25863/api/contracts/integration',
       function (err, response, body) {
@@ -63,7 +90,7 @@ describe('Given a jackal and a provider', function() {
     )
   })
 
-  it('Should pass if one passing build', function(done) {
+  it('Sending contract-v2 should pass', function(done) {
     consumer.send(
         'test/integration/contract-v2.json',
         'http://localhost:25863/api/contracts',
@@ -77,5 +104,17 @@ describe('Given a jackal and a provider', function() {
           done()
         }
       )
+  })
+
+  it('Running contract-v2 the first time should pass', function(done) {
+    runContract(done)
+  })
+
+  it('Running contract-v2 a second time should pass', function(done) {
+    runContract(done)
+  })
+
+  it('Running contract-v2 a third time should pass', function(done) {
+    runContract(done)
   })
 })
