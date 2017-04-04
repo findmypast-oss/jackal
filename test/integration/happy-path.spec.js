490e5eb6281dae0a09ebd2d5fccf6ce5cfa9bf78
Donald Robertson <drobertson@findmypast.com>
2017-04-04 09:35:57 +0100

    Uncommented API tests

--- test/integration/happy-path.spec.js
+++ test/integration/happy-path.spec.js
@@ -13,51 +13,51 @@ describe('Happy path tests', function() {
     provider.start({ port: 5000, contract: { version: '1' } }, done)
   })
 
-  xit('Sending contract-v1 the first time should pass', function(done) {
+  it('Sending contract-v1 the first time should pass', function(done) {
     client.send({ filePath: 'test/integration/contract-v1.json', isPass: true }, done)
   })
 
-  xit('Sending contract-v1 a second time should pass', function(done) {
+  it('Sending contract-v1 a second time should pass', function(done) {
     client.send({ filePath: 'test/integration/contract-v1.json', isPass: true }, done)
   })
 
-  xit('Sending contract-v1 a third time should pass', function(done) {
+  it('Sending contract-v1 a third time should pass', function(done) {
     client.send({ filePath: 'test/integration/contract-v1.json', isPass: true }, done)
   })
 
-  xit('Jackal should have hit "/contract" 3 times', function() {
+  it('Jackal should have hit "/contract" 3 times', function() {
     expect(provider.contractHitCount()).to.equal(3)
   })
 
-  xit('The provider should stop successfully', function(done) {
+  it('The provider should stop successfully', function(done) {
     provider.stop(done)
   })
 
-  xit('The provider should start successfully with contract-v2', function(done){
+  it('The provider should start successfully with contract-v2', function(done){
     provider.start({ port: 5000, contract: { version: 'abc' } }, done)
   })
 
-  xit('The existing contract-v1 should fail for the provider with contract-v2', function(done){
+  it('The existing contract-v1 should fail for the provider with contract-v2', function(done){
     client.run({ provider: 'integration', isPass: false }, done)
   })
 
-  xit('Sending contract-v2 should pass', function(done) {
+  it('Sending contract-v2 should pass', function(done) {
     client.send({ filePath: 'test/integration/contract-v2.json', isPass: true }, done)
   })
 
-  xit('Running contract-v2 the first time should pass', function(done) {
+  it('Running contract-v2 the first time should pass', function(done) {
     client.run({ provider: 'integration', isPass: true }, done)
   })
 
-  xit('Running contract-v2 a second time should pass', function(done) {
+  it('Running contract-v2 a second time should pass', function(done) {
     client.run({ provider: 'integration', isPass: true }, done)
   })
 
-  xit('Running contract-v2 a third time should pass', function(done) {
+  it('Running contract-v2 a third time should pass', function(done) {
     client.run({ provider: 'integration', isPass: true }, done)
   })
 
-  xit('Jackal should have hit "/contract" 5 times', function() {
+  it('Jackal should have hit "/contract" 5 times', function() {
     expect(provider.contractHitCount()).to.equal(5)
   })
 })
