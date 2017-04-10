5c3698d2e9edf7f064426013701d85a1c046a8cf
Neil Crawford <neil.crawford@gmail.com>
2017-04-10 20:29:43 +0100

    Enabling all tests

--- test/integration/multiple-consumers.spec.js
+++ test/integration/multiple-consumers.spec.js
@@ -12,7 +12,7 @@ const assertAllContractResultsPassWithCount = (count, done) => (err, results) =>
   done()
 }
 
-describe.only('Multiple Consumers, One Provider', function () {
+describe('Multiple Consumers, One Provider', function () {
   before((done) => jackal.start({}, done))
   after(jackal.stop)
   after(provider.stop)
