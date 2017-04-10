470a5074b0cd0ee221bc1960f4e27542a2688c20
Neil Crawford <neil.crawford@gmail.com>
2017-04-10 21:18:33 +0100

    Tidying tests

--- test/program/helpers/jackal.js
+++ test/program/helpers/jackal.js
@@ -5,10 +5,8 @@ const startServer = require('../../../server')
 let server = null
 const config = {
   db:     { path: 'db.json' },
-  logger: { environment: 'production' },
-  statsD: { host: 'localhost', port: 8125, prefix: 'jackal' },
-  quiet:  true,
-  jackal: { }
+  jackal: { port: 25863 },
+  quiet:  true
 }
 
 const start = (port) => (done) => {
--- test/uat/jackal.js
+++ test/uat/jackal.js
@@ -5,8 +5,6 @@ const startServer = require('../../server')
 let server = null
 const config = {
   db:     { path: 'db.json' },
-  logger: { environment: 'production' },
-  statsD: { host: 'localhost', port: 8125, prefix: 'jackal' },
   jackal: { host: 'http://localhost', port: 25863},
   quiet:  false
 }
