225f82c925dc76081fd89ddfcf06928df484355f
Neil Crawford <neil.crawford@gmail.com>
2017-04-11 10:47:02 +0100

    Changing dump endpoint from /api/contracts to /api/db

--- client/dump.js
+++ client/dump.js
@@ -5,6 +5,6 @@ const parser = require('./response-standard')
 const url = require('./jackal-url')
 
 module.exports = (jackalUrl, options, done) => {
-  const jacky = url(jackalUrl, '/api/contracts')
+  const jacky = url(jackalUrl, '/api/db')
   request(jacky, parser(done))
 }
