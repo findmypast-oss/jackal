1cf71d4323d03d42a6d4b51a35b363e6ad372c7b
Donald Robertson <drobertson@findmypast.com>
2017-04-12 11:13:29 +0100

    Moved integration test helpers and contracts

---
+++ test/integration/helpers/jackal.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const startServer = require('../../../server')
+
+let server
+
+module.exports = {
+  start:  (options, done) => { server = startServer(options, done) },
+  stop:   (done)          => { server.close(done) }
+}
