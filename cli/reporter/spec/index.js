679c0bd0d93f89d78c3e95d3a9f1fb5a8ce6c187
Donald Robertson <drobertson@findmypast.com>
2017-04-13 15:11:46 +0100

    Fixing CLI Send tests

--- cli/reporter/spec/index.js
+++ cli/reporter/spec/index.js
@@ -1,8 +1,5 @@
 'use strict'
 
-// TODO: Consider revising this module, _especially_ the exported function with
-// the if... elseif... else block
-
 const flattenDeep = require('lodash/flattenDeep')
 const processConsumerLogs = require('./process-consumer-logs')
 const processValidationLogs = require('./process-validation-logs')
@@ -17,85 +14,13 @@ const actions = {
 }
 
 const handleConsumerLogs = (body) => {
-  const provider = body[0].name.split('/')[0]
+  const provider = body.results[0].name.split('/')[0]
 
-  return processConsumerLogs(body, provider)
+  return processConsumerLogs(body.results, provider)
 }
 
 module.exports = (err, response, body) => {
   const logs = actions[body.status](body)
 
-  // if (typeof response === 'string' && response.startsWith('Skipping')) {
-  //   logs.push(response)
-  // } else if (body.message && body.validations) {
-  //   logs.push(body.message)
-  //   logs.push(processValidationLogs(body.validations))
-  // } else if (body.message) {
-  //   logs.push(body.message)
-  // } else if (body.length === 0) {
-  //   logs.push('Failure - no contracts exist')
-  // } else {
-  //
-  //
-  //   if (body.some(result => result.status === 'Fail')) {
-  //     logs.push('Failure - not all contracts passed')
-  //   }
-  //
-  //   logs.push()
-  // }
-
   return flattenDeep(logs)
 }
-
-
-// // Skipped contracts
-// response = null
-// body = {
-//   message: 'Skipping because of blah blah blah',
-//   status: 'SKIPPED'
-//   results: []
-// }
-//
-// // Some Error
-// response = server.response
-// body = {
-//   message: 'Some error happened',
-//   status: 'ERROR'
-//   results: []
-// }
-//
-// // Invalid contracts
-// response = server.response
-// body = {
-//   message: 'One or more contracts are invalid',
-//   status: 'INVALID'
-//   results: [
-//     { validation_one },
-//     { validation_two },
-//     { etc }
-//   ]
-// }
-//
-// // Failing contracts
-// response = server.response
-// body = {
-//   message: 'Not all contracts passed',
-//   status: 'FAILED'
-//   results: [
-//     { result_one },
-//     { result_two },
-//     { etc }
-//   ]
-// }
-//
-// // Passing contracts
-// response = server.response
-// body = {
-//   message: 'All contracts passed',
-//   status: 'PASSED'
-//   results: [
-//     { result_one },
-//     { result_two },
-//     { etc }
-//   ]
-// }
--- cli/reporter/teamcity/index.js
+++ cli/reporter/teamcity/index.js
@@ -1,22 +1,25 @@
 'use strict'
 
-// TODO: Consider revising this module, _especially_ the exported function with
-// the if... elseif... else block
-
 const flattenDeep = require('lodash/flattenDeep')
 const processConsumerLogs = require('./process-consumer-logs')
 
-module.exports = (err, response, body) => {
-  let logs = []
+const actions = {
+  SKIPPED:      (body) => [ body.message ],
+  ERROR:        (body) => [ body.message ],
+  NO_CONTRACTS: (body) => [ body.message ],
+  INVALID:      (body) => [ body.message, JSON.stringify(body.results) ],
+  FAILED:       (body) => [ handleConsumerLogs(body) ],
+  PASSED:       (body) => [ handleConsumerLogs(body) ]
+}
 
-  if (typeof response === 'string' && response.startsWith('Skipping')) {
-    logs.push(response)
-  } else if (body.message) {
-    logs.push(JSON.stringify(body))
-  } else {
-    const provider = body[0].name.split('/')[0]
-    logs.push(processConsumerLogs(body, provider))
-  }
+const handleConsumerLogs = (body) => {
+  const provider = body.results[0].name.split('/')[0]
+
+  return processConsumerLogs(body.results, provider)
+}
+
+module.exports = (err, response, body) => {
+  const logs = actions[body.status](body)
 
   return flattenDeep(logs)
 }
