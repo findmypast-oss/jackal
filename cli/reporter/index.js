4710beab0b62212a9aaf57563c423d99e99c2f66
Sash <sashman90@gmail.com>
2017-04-28 14:41:06 +0100

    Adding a warning print if the minor version doesnt match between the client and the server

--- cli/reporter/index.js
+++ cli/reporter/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const pkgInfo = require('../../package.json')
+const chalk = require('chalk')
 const spec = require('./spec')
 const pretty = require('./pretty')
 const json = require('./json')
@@ -19,7 +21,32 @@ const statusErrorMap = {
 }
 
 
+const minorVersionMatches = (clientVersion, serverVersion) => {
+  if (!clientVersion || !serverVersion) {
+    return false
+  }
+
+  const clientVersionArray = clientVersion.split('.')
+  const serverVersionArray = serverVersion.split('.')
+
+  return clientVersionArray[0] === serverVersionArray[0] && clientVersionArray[1] === serverVersionArray[1]
+}
+
+const printVersionMatchWarning = (response) => {
+  if (response) {
+    const clientVersion = pkgInfo.version
+    const serverVersion = response.headers['jackal-server-version']
+    if (!minorVersionMatches(clientVersion, serverVersion)) {
+      /* eslint-disable no-console  */
+      console.error(chalk.yellow(`Warning! Client Version ${clientVersion} does not match server version ${serverVersion}`))
+      /* eslint-enble no-console  */
+    }
+  }
+}
+
 module.exports = (reporters, reporterOption, done) => (err, response, body) => {
+  printVersionMatchWarning(response)
+
   if (err) {
     return done(err, response, body)
   }
