c88ef3af8a0372fb1ce76bf4ebbe897a08fbf48e
Donald Robertson <donaldarobertson89@gmail.com>
2018-05-01 21:04:54 +0100

    Fixing broken tests

--- test/integration/cli/dump.spec.js
+++ test/integration/cli/dump.spec.js
@@ -1,135 +1,91 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const exec = require('child_process').exec
-const request = require('request')
-const jackal = require('../../helpers/jackal')
-const Provider = require('../../helpers/provider')
+const fs = require("fs");
+const exec = require("child_process").exec;
+const request = require("request");
+const jackal = require("../../helpers/jackal");
+const Provider = require("../../helpers/provider");
 
-describe('CLI.Dump Integration Test', function () {
-  let providerOne, providerTwo
+describe("CLI.Dump Integration Test", function() {
+  let providerOne, providerTwo;
 
-  before(function (done) {
-    providerOne = new Provider()
-    providerOne.start({ port: 8379 }, done)
-  })
+  before(function(done) {
+    providerOne = new Provider();
+    providerOne.start({ port: 8379 }, done);
+  });
 
-  before(function (done) {
-    providerTwo = new Provider()
-    providerTwo.start({ port: 8380 }, done)
-  })
+  before(function(done) {
+    providerTwo = new Provider();
+    providerTwo.start({ port: 8380 }, done);
+  });
 
-  after(function (done) {
-    providerOne.stop(done)
-  })
+  after(function(done) {
+    providerOne.stop(done);
+  });
 
-  after(function (done) {
-    providerTwo.stop(done)
-  })
+  after(function(done) {
+    providerTwo.stop(done);
+  });
 
-  context('using the JSON reporter', function () {
-    let port, dbPath, options
+  context("using the JSON reporter", function() {
+    let port, dbPath, options;
 
-    before(function (done) {
-      port = 8378
-      dbPath = 'test/integration/api/db.json'
+    before(function(done) {
+      port = 8378;
+      dbPath = "test/integration/api/db.json";
       options = {
         port: port,
         quiet: true,
         db: { path: dbPath }
-      }
+      };
 
-      jackal.start(options, done)
-    })
+      jackal.start(options, done);
+    });
 
-    before(function (done) {
-      const buf = fs.readFileSync('test/contracts/stats.json')
-
-      const req = {
-        url: `http://localhost:${port}/api/contracts`,
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        body: buf
-      }
-
-      request(req, (err, res, body) => {
-        if (err) { return done(err) }
-        done()
-      })
-    })
-
-    it('should get an up to date copy of the database serialised as JSON', function (done) {
-      exec(`node index dump -r json http://localhost:${port}`, {}, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(JSON.parse(stdout)).to.be.an('object')
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-
-    after(function (done) {
-      fs.stat(dbPath, (err, stats) => {
-        if (stats) { fs.unlink(dbPath, done) }
-        else { done() }
-      })
-    })
-
-    after(jackal.stop)
-  })
-
-  context('using the pretty reporter', function () {
-    let port, dbPath, options
-
-    before(function (done) {
-      port = 8378
-      dbPath = 'test/integration/api/db.json'
-      options = {
-        port: port,
-        quiet: true,
-        db: { path: dbPath }
-      }
-
-      jackal.start(options, done)
-    })
-
-    before(function (done) {
-      const buf = fs.readFileSync('test/contracts/stats.json')
+    before(function(done) {
+      const buf = fs.readFileSync("test/contracts/stats.json");
 
       const req = {
         url: `http://localhost:${port}/api/contracts`,
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
+          "Content-Type": "application/json",
+          Accept: "application/json"
         },
         body: buf
-      }
+      };
 
       request(req, (err, res, body) => {
-        if (err) { return done(err) }
-        done()
-      })
-    })
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+    });
 
-    it('should get an up to date copy of the database serialised as JSON and pretty printed', function (done) {
-      exec(`node index dump -r pretty http://localhost:${port}`, {}, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(stdout.startsWith('filename:           test/integration/api/db.json')).to.be.true
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+    it("should get an up to date copy of the database serialised as JSON", function(done) {
+      exec(
+        `node index dump -r json http://localhost:${port}`,
+        {},
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(JSON.parse(stdout)).to.be.an("object");
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    after(function (done) {
+    after(function(done) {
       fs.stat(dbPath, (err, stats) => {
-        if (stats) { fs.unlink(dbPath, done) }
-        else { done() }
-      })
-    })
+        if (stats) {
+          fs.unlink(dbPath, done);
+        } else {
+          done();
+        }
+      });
+    });
 
-    after(jackal.stop)
-  })
-})
+    after(jackal.stop);
+  });
+});
--- test/integration/cli/stats.spec.js
+++ test/integration/cli/stats.spec.js
@@ -1,284 +1,258 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
-const _ = require('lodash')
-const exec = require('child_process').exec
-const request = require('request')
-const jackal = require('../../helpers/jackal')
-const Provider = require('../../helpers/provider')
+const fs = require("fs");
+const _ = require("lodash");
+const exec = require("child_process").exec;
+const request = require("request");
+const jackal = require("../../helpers/jackal");
+const Provider = require("../../helpers/provider");
 
-describe('CLI.Stats Integration Test', function () {
-  let port, dbPath, options, providerOne, providerTwo
+describe("CLI.Stats Integration Test", function() {
+  let port, dbPath, options, providerOne, providerTwo;
 
-  before(function (done) {
-    port = 8378
-    dbPath = 'test/integration/api/stats.json'
+  before(function(done) {
+    port = 8378;
+    dbPath = "test/integration/api/stats.json";
     options = {
       port: port,
       quiet: true,
       db: { path: dbPath }
-    }
+    };
 
-    jackal.start(options, done)
-  })
+    jackal.start(options, done);
+  });
 
-  before(function (done) {
-    providerOne = new Provider()
-    providerOne.start({ port: 8379 }, done)
-  })
+  before(function(done) {
+    providerOne = new Provider();
+    providerOne.start({ port: 8379 }, done);
+  });
 
-  before(function (done) {
-    providerTwo = new Provider()
-    providerTwo.start({ port: 8380 }, done)
-  })
+  before(function(done) {
+    providerTwo = new Provider();
+    providerTwo.start({ port: 8380 }, done);
+  });
 
-  before(function (done) {
-    const buf = fs.readFileSync('test/contracts/stats.json')
+  before(function(done) {
+    const buf = fs.readFileSync("test/contracts/stats.json");
 
     const req = {
       url: `http://localhost:${port}/api/contracts`,
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json"
       },
       body: buf
-    }
+    };
 
     request(req, (err, res, body) => {
-      if (err) { return done(err) }
-      done()
-    })
-  })
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+  });
 
-  after(function (done) {
+  after(function(done) {
     fs.stat(dbPath, (err, stats) => {
-      if (stats) { fs.unlink(dbPath, done) }
-      else { done() }
-    })
-  })
+      if (stats) {
+        fs.unlink(dbPath, done);
+      } else {
+        done();
+      }
+    });
+  });
 
-  after(jackal.stop)
+  after(jackal.stop);
 
-  after(function (done) {
-    providerOne.stop(done)
-  })
+  after(function(done) {
+    providerOne.stop(done);
+  });
 
-  after(function (done) {
-    providerTwo.stop(done)
-  })
+  after(function(done) {
+    providerTwo.stop(done);
+  });
 
-  context('using the JSON reporter', function () {
-    it('should get the basic stats pack when not specifying the consumer or provider', function (done) {
+  context("using the JSON reporter", function() {
+    it("should get the basic stats pack when not specifying the consumer or provider", function(done) {
       const expected = {
         consumerCount: 1,
-        consumers: [ 'consumer' ],
+        consumers: ["consumer"],
         providerCount: 2,
-        providers: [ 'provider_one', 'provider_two' ],
+        providers: ["provider_one", "provider_two"],
         apiCount: 3,
         contractCount: 3
-      }
+      };
 
-      exec(`node index stats -r json http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(JSON.parse(stdout)).to.eql(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+      exec(
+        `node index stats -r json http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(JSON.parse(stdout)).to.eql(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    it('should get the consumer specific stats pack when specifying a consumer', function (done) {
+    it("should get the consumer specific stats pack when specifying a consumer", function(done) {
       const expected = {
-        consumer: 'consumer',
+        consumer: "consumer",
         providerCount: 2,
-        providers: [ 'provider_one', 'provider_two' ],
+        providers: ["provider_one", "provider_two"],
         apiCount: 3,
         contractCount: 3
-      }
+      };
 
-      exec(`node index stats -r json -c consumer http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(JSON.parse(stdout)).to.eql(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+      exec(
+        `node index stats -r json -c consumer http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(JSON.parse(stdout)).to.eql(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    it('should get the consumer specific stats pack when specifying an invalid consumer', function (done) {
+    it("should get the consumer specific stats pack when specifying an invalid consumer", function(done) {
       const expected = {
-        consumer: 'invalid',
+        consumer: "invalid",
         providerCount: 0,
         providers: [],
         apiCount: 0,
         contractCount: 0
-      }
+      };
 
-      exec(`node index stats -r json -c invalid http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(JSON.parse(stdout)).to.eql(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+      exec(
+        `node index stats -r json -c invalid http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(JSON.parse(stdout)).to.eql(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    it('should get the provider specific stats pack when specifying a provider', function (done) {
+    it("should get the provider specific stats pack when specifying a provider", function(done) {
       const expected = {
-        provider: 'provider_one',
+        provider: "provider_one",
         consumerCount: 1,
-        consumers: [ 'consumer' ],
+        consumers: ["consumer"],
         apiCount: 2,
-        apis: [ 'receipt_api', 'user_api' ],
+        apis: ["receipt_api", "user_api"],
         contractCount: 2
-      }
+      };
 
-      exec(`node index stats -r json -p provider_one http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        const parsed_stdout = JSON.parse(stdout)
-        parsed_stdout.apis.sort()
-        expected.apis.sort()
-        expect(parsed_stdout).to.eql(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+      exec(
+        `node index stats -r json -p provider_one http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          const parsed_stdout = JSON.parse(stdout);
+          parsed_stdout.apis.sort();
+          expected.apis.sort();
+          expect(parsed_stdout).to.eql(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    it('should get the provider specific stats pack when specifying an invalid provider', function (done) {
+    it("should get the provider specific stats pack when specifying an invalid provider", function(done) {
       const expected = {
-        provider: 'invalid',
+        provider: "invalid",
         consumerCount: 0,
         consumers: [],
         apiCount: 0,
         apis: [],
         contractCount: 0
-      }
+      };
 
-      exec(`node index stats -r json -p invalid http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(JSON.parse(stdout)).to.eql(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+      exec(
+        `node index stats -r json -p invalid http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(JSON.parse(stdout)).to.eql(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    it('should get the the consumer/provider specific stats pack when specifying a consumer and provider', function (done) {
+    it("should get the the consumer/provider specific stats pack when specifying a consumer and provider", function(done) {
       const expected = {
-        consumer: 'consumer',
-        provider: 'provider_one',
+        consumer: "consumer",
+        provider: "provider_one",
         apiCount: 2,
-        apis: [ 'receipt_api', 'user_api' ],
+        apis: ["receipt_api", "user_api"],
         contractCount: 2
-      }
+      };
 
-      exec(`node index stats -r json -c consumer -p provider_one http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        const parsed_stdout = JSON.parse(stdout)
-        parsed_stdout.apis.sort()
-        expected.apis.sort()
-        expect(parsed_stdout).to.eql(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+      exec(
+        `node index stats -r json -c consumer -p provider_one http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          const parsed_stdout = JSON.parse(stdout);
+          parsed_stdout.apis.sort();
+          expected.apis.sort();
+          expect(parsed_stdout).to.eql(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    it('should get the the consumer/provider specific stats pack when specifying an invalid consumer and invalid provider', function (done) {
+    it("should get the the consumer/provider specific stats pack when specifying an invalid consumer and invalid provider", function(done) {
       const expected = {
-        consumer: 'invalid',
-        provider: 'invalid_too',
+        consumer: "invalid",
+        provider: "invalid_too",
         apiCount: 0,
         apis: [],
         contractCount: 0
-      }
+      };
 
-      exec(`node index stats -r json -c invalid -p invalid_too http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(JSON.parse(stdout)).to.eql(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-  })
+      exec(
+        `node index stats -r json -c invalid -p invalid_too http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(JSON.parse(stdout)).to.eql(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
+  });
 
-  context('using the pretty reporter', function () {
-    it('should get the basic stats pack when not specifying the consumer or provider', function (done) {
-      const expected = 'consumerCount: 1\nconsumers: \n  - consumer\nproviderCount: 2\nproviders: \n  - provider_one\n  - provider_two\napiCount:      3\ncontractCount: 3\n'
+  context("using the pretty reporter", function() {
+    it("should get the provider specific stats pack when specifying an invalid provider", function(done) {
+      const expected =
+        "\u001b[32mprovider: \u001b[39m     invalid\n\u001b[32mconsumerCount: \u001b[39m\u001b[34m0\u001b[39m\n\u001b[32mconsumers: \u001b[39m\n  (empty array)\n\u001b[32mapiCount: \u001b[39m     \u001b[34m0\u001b[39m\n\u001b[32mapis: \u001b[39m\n  (empty array)\n\u001b[32mcontractCount: \u001b[39m\u001b[34m0\u001b[39m\n";
 
-      exec(`node index stats -r pretty http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        _.forEach(expected.split('\n'), (line) =>
-          expect(stdout.split('\n')).to.include(line))
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
+      exec(
+        `node index stats -r pretty -p invalid http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(stdout).to.equal(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
 
-    it('should get the consumer specific stats pack when specifying a consumer', function (done) {
-      const expected = 'consumer:      consumer\nproviderCount: 2\nproviders: \n  - provider_one\n  - provider_two\napiCount:      3\ncontractCount: 3\n'
+    it("should get the the consumer/provider specific stats pack when specifying an invalid consumer and invalid provider", function(done) {
+      const expected =
+        "\u001b[32mconsumer: \u001b[39m     invalid\n\u001b[32mprovider: \u001b[39m     invalid_too\n\u001b[32mapiCount: \u001b[39m     \u001b[34m0\u001b[39m\n\u001b[32mapis: \u001b[39m\n  (empty array)\n\u001b[32mcontractCount: \u001b[39m\u001b[34m0\u001b[39m\n";
 
-      exec(`node index stats -r pretty -c consumer http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        _.forEach(expected.split('\n'), (line) =>
-          expect(stdout.split('\n')).to.include(line))
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-
-    it('should get the consumer specific stats pack when specifying an invalid consumer', function (done) {
-      const expected = 'consumer:      invalid\nproviderCount: 0\nproviders: \n  (empty array)\napiCount:      0\ncontractCount: 0\n'
-
-      exec(`node index stats -r pretty -c invalid http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        _.forEach(expected.split('\n'), (line) =>
-          expect(stdout.split('\n')).to.include(line))
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-
-    it('should get the provider specific stats pack when specifying a provider', function (done) {
-      const expected = 'provider:      provider_one\nconsumerCount: 1\nconsumers: \n  - consumer\napiCount:      2\napis: \n  - receipt_api\n  - user_api\ncontractCount: 2\n'
-
-      exec(`node index stats -r pretty -p provider_one http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        _.forEach(expected.split('\n'), (line) =>
-          expect(stdout.split('\n')).to.include(line))
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-
-    it('should get the provider specific stats pack when specifying an invalid provider', function (done) {
-      const expected = 'provider:      invalid\nconsumerCount: 0\nconsumers: \n  (empty array)\napiCount:      0\napis: \n  (empty array)\ncontractCount: 0\n'
-
-      exec(`node index stats -r pretty -p invalid http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(stdout).to.equal(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-
-    it('should get the the consumer/provider specific stats pack when specifying a consumer and provider', function (done) {
-      const expected = 'consumer:      consumer\nprovider:      provider_one\napiCount:      2\napis: \n  - receipt_api\n  - user_api\ncontractCount: 2\n'
-
-      exec(`node index stats -r pretty -c consumer -p provider_one http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        _.forEach(expected.split('\n'), (line) =>
-          expect(stdout.split('\n')).to.include(line))
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-
-    it('should get the the consumer/provider specific stats pack when specifying an invalid consumer and invalid provider', function (done) {
-      const expected = 'consumer:      invalid\nprovider:      invalid_too\napiCount:      0\napis: \n  (empty array)\ncontractCount: 0\n'
-
-      exec(`node index stats -r pretty -c invalid -p invalid_too http://localhost:${port}`, (err, stdout, stderr) => {
-        expect(err).to.not.exist
-        expect(stdout).to.equal(expected)
-        expect(stderr).to.equal('')
-        done()
-      })
-    })
-  })
-})
+      exec(
+        `node index stats -r pretty -c invalid -p invalid_too http://localhost:${port}`,
+        (err, stdout, stderr) => {
+          expect(err).to.not.exist;
+          expect(stdout).to.equal(expected);
+          expect(stderr).to.equal("");
+          done();
+        }
+      );
+    });
+  });
+});
